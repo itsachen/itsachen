f8a4417cb0cbca6e432a302129c77760b859ee60
Anthony Chen <pandaman0529@gmail.com>
2012-06-20 11:55:00 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itsachen::Application.config.secret_token = '4f851a3608a61c12a75cd6312333997512fec15186f0721a7264122ef767888cc04e3fcc7d8be08437199d63c9de0117c72e1b0bee60c972d607d3e0ea73af44'
